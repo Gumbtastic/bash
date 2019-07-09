1e98509fc04ff2021e4ded12ee2e2862009dd867
Gumbtastic <c.gumbert@gmx.net>
2019-07-09 22:54:24 +0200

    first build

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1562705470,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/peer-sheet0.png",
+		"images/shield-sheet0.png",
+		"images/peerfade-sheet0.png",
+		"images/map.png",
+		"images/textbar__playersalive-sheet0.png",
+		"images/textbar__room-sheet0.png",
+		"images/staminabar__bg-sheet0.png",
+		"images/staminabar.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
